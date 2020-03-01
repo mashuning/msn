37cc94b9f5a0ef2f4ac21b015ac2295baf2566e6
mashuning <630029203@qq.com>
2020-03-01 16:21:25 +0800

    添加异步task任务

---
+++ core/lib/task/Task.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 代表的是  swoole里面 后续 所有  task异步 任务 都放这里来
+ * Date: 18/3/27
+ * Time: 上午1:20
+ */
+namespace core\lib\task;
+use core\lib\log;
+
+class Task {
+
+
+    /**
+     * @param $data
+     * @param $serv swoole server对象
+     */
+    public function test($data, $serv){
+        /**
+         *  todo
+         */
+//        log::log();
+    }
+}
